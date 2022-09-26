57530c429495501ac4510d827f4c4758b0edd247
Rachelli Isela Matias Nuñez <rachelliisela303@gmail.com>
2022-09-26 11:49:56 -0400

    Complete Proyect CRUDFOOD

---
+++ CrudFoodMVC/CrudFood/CrudFood/Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudFood.Models
+{
+    public partial class Food
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        [Range(1,5)]
+        public int? Rating { get; set; }
+    }
+}
